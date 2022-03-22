75c10a7441f48e5a0f0464e87a385d3d5e3fe2e7
hieubui17b8 <hieubui17thtb@gmail.com>
2022-03-22 15:53:40 +0700

    chapter0506
    chapter0506

---
+++ chapter05/vd1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	float p, n, r;
+	system("cls");
+	p = 1000;
+	n = 2.5;
+	r = 10.5;
+	printf("\n Amount is %f", p*n*r/100);
+	return 0;
+}
---
+++ chapter05/vd2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+int a,b,c,d;
+system("cls");
+a = 50;
+b = 24;
+c = 68;
+d = a*b+c/2;
+printf("\n The value after a*b+c/2 is: %d", d);
+d = a%b;
+printf("\n The value after a mod b is: %d", d);
+d = a*b-c;
+printf("\n The value after a*b-c is: %d", d);
+d = a/b+c;
+printf("\n The value after a/b+c is: %d", d);
+d = a+b*c;
+printf("\n The value after a+b*c is: %d", d);
+d = (a+b)*c;
+printf("\n The value after (a+b)*c is: %d", d);
+d = a*(b+c+(a-c)*b);
+printf("\n The value after a*(b+c+(a-c)*b) is: %d", d);
+}
